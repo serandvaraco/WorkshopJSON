ccc0b19ffabb3f6f20167f8a6a0ec28fef702a9c
Sergio Vargas Acosta <serandvaraco@outlook.com>
2018-08-17 10:17:45 -0500

    Add Demos

---
+++ Demos/WorkshopJSONXML/WorkshopJSON/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace WorkshopJSON
+{
+    using System;
+    using System.Collections.Generic;
+    using ServiceStack;
+    using ServiceStack.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var movies = new[]
+            {
+                new { name = "Movie 1", year = 1985 },
+                new { name = "Movie 2", year = 1986 }
+
+            };
+
+
+            var json = "{\"id\": 1, \"name\": \"sergio\"}";
+
+            //Option 1
+            var obj = JsonObject.Parse(json);
+            var id = obj.Get<int>("id").ToString();
+            var name = obj.Get<string>("name");
+            //Console.WriteLine($"Id= {id} Name = {name}");
+
+            //option 2
+            dynamic dyn = DynamicJson.Deserialize(json);
+            //Console.WriteLine($"Dinámico {dyn.id} {dyn.name}");
+
+            //Option 3
+            JsConfig.ConvertObjectTypesIntoStringDictionary = true;
+            var map = (Dictionary<string, object>)json.FromJson<object>();
+            map.PrintDump(); 
+            /*
+            var idMap = map["id"];
+            var nameMap = map["name"]; 
+            */
+
+
+
+            //Console.WriteLine(movies.ToJson());
+
+            Console.ReadKey();
+            Console.WriteLine("FIN");
+
+        }
+    }
+}
---
+++ Demos/WorkshopJSONXML/WorkshopJsonDemo2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace WorkshopJsonDemo2
+{
+    using System;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var movies = new[]
+           {
+                new { name = "Movie 1", year = 1985 },
+                new { name = "Movie 2", year = 1986 }
+
+            };
+
+            var json=   JsonConvert.SerializeObject(movies);
+            //Console.WriteLine(json);
+
+            var jsonString = "{\"id\": 1, \"name\": \"sergio\"}";
+
+            var objectJson = JsonConvert.DeserializeAnonymousType(jsonString,
+                new { id = 0, name = "" });
+
+            Console.WriteLine($"Id= {objectJson.id}, Name = {objectJson.name}");
+
+            Console.ReadKey();
+            Console.WriteLine("FIN ");
+        }
+    }
+}
