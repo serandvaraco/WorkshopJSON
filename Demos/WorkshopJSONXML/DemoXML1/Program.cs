b3471c193bccb30ce8f64533c58d778189bac585
Sergio Vargas Acosta <serandvaraco@outlook.com>
2018-09-04 20:54:54 -0500

    workshop demos

---
+++ Demos/WorkshopJSONXML/DemoXML1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+namespace DemoXML1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XDocument doc = XDocument.Load("Catalog.xml");
+            XElement catalog = doc.Element("Catalog");
+
+            IEnumerable<XElement> books = catalog.Elements("Book");
+
+            foreach (XElement book in books)
+            {
+                Console.Write($"\t{book.Attribute("id").Value}");
+                Console.WriteLine("---");
+                Console.Write($"Title: {book.Element("Title").Value} Author: { book.Element("Author").Value}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
