84c1aeca6a5ba07510b32e053d9e99b5d286e1d6
Sergio Vargas Acosta <serandvaraco@outlook.com>
2018-09-04 21:16:54 -0500

    Updated XML projects

---
+++ Demos/WorkshopJSONXML/SchemeXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace SchemeXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", "CustomerOrder.xsd");
+            XDocument customerOrder = XDocument.Load("CustomerOrder.xml");
+            bool errors = false;
+            customerOrder.Validate(schema, (o, a) =>
+            {
+                Console.WriteLine($"{a.Message}");
+                errors = true;
+            });
+            if (errors)
+            {
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
---
+++ Demos/WorkshopJSONXML/UpdateXML/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace UpdateXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XDocument doc = XDocument.Load("Catalog.xml");
+            XElement catalog = doc.Element("Catalog");
+
+            IEnumerable<XElement> books = catalog.Elements("Book");
+
+            foreach (XElement book in books)
+            {
+                Console.Write($"\t{book.Attribute("id").Value}");
+                Console.WriteLine("---");
+                Console.Write($"Title: {book.Element("Title").Value} Author: { book.Element("Author").Value}");
+
+                book.Element("Title").Value = "HACK"; 
+            }
+
+            doc.Save("CatalogHACK!!!");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
